1b6702d2879bf6aaa3e4e22fd0a12ebade56486a
krasnokutskiy.e <Krasnokutskiy.E@ostec-group.ru>
2021-11-16 02:57:21 +0300

    WIP

--- src/pages/404/404.js
+++ src/pages/404/404.js
@@ -1,13 +1,17 @@
 import compileTemplate from './404.pug';
 import '../../assets/styles/global.scss';
 import pageNotFoundLogo from '../../assets/images/ghost.svg'
+
+// parent and children styles
 import * as classes from  './404.module.scss';
-
-
+import * as parentClasses from  '../../templates/centerChildren/centerChildren.module.scss';
+import * as errorMessageClasses from '../../components/errorMessage/errorMessage.module.scss';
 
 // 1 - generate context
 const ctx = {
   classes,
+  parentClasses,
+  errorMessageClasses,
   pageNotFoundLogo
 }
 
