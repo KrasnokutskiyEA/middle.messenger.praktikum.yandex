480b8dc525243454d4059dd3e6eda79c2be37a98
krasnokutskiy.e <Krasnokutskiy.E@ostec-group.ru>
2021-11-16 19:58:57 +0300

    added 500 page

---
+++ src/pages/500/500.js
@@ -0,0 +1,20 @@
+import compileTemplate from './500.pug';
+import '../../assets/styles/global.scss';
+import serverErrorLogo from '../../assets/images/fire.svg'
+
+// parent and children styles
+import * as classes from  './500.module.scss';
+import * as parentClasses from  '../../templates/centerChildren/centerChildren.module.scss';
+import * as errorMessageClasses from '../../components/errorMessage/errorMessage.module.scss';
+
+// 1 - generate context
+const ctx = {
+  classes,
+  parentClasses,
+  errorMessageClasses,
+  serverErrorLogo
+}
+
+// 2 - generate markup
+document.body.innerHTML = ''
+document.body.insertAdjacentHTML('afterbegin', compileTemplate(ctx));
