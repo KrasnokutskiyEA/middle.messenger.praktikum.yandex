50d4853a3446796eb572ae6778f1a27de27501be
krasnokutskiy.e <Krasnokutskiy.E@ostec-group.ru>
2021-11-17 04:19:57 +0300

    WIP

--- src/pages/signIn/signIn.js
+++ src/pages/signIn/signIn.js
@@ -5,12 +5,14 @@ import '../../assets/styles/global.scss';
 import * as templateStyles from  '../../templates/centerChildren/centerChildren.module.scss';
 import * as moduleSignInStyles from '../../modules/credentialsForm/credentialsForm.module.scss';
 import * as componentPrimaryBtnStyles from '../../components/primaryBtn/primaryBtn.module.scss';
+import * as componentTextFieldStyles from '../../components/textField/textField.module.scss';
 
 // 1 - generate context
 const ctx = {
   templateStyles,
   moduleSignInStyles,
-  componentPrimaryBtnStyles
+  componentPrimaryBtnStyles,
+  componentTextFieldStyles
 }
 
 // 2 - generate markup
