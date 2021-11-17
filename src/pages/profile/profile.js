901122b0a35c97a27ba0b2458ccff1fc56a62db2
krasnokutskiy.e <Krasnokutskiy.E@ostec-group.ru>
2021-11-17 18:47:15 +0300

    WIP

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -4,6 +4,7 @@ import avatarLogo from '../../assets/images/avatar.svg'
 // parent and children styles
 import '../../assets/styles/global.scss';
 import * as templateStyles from  '../../templates/centerChildren/centerChildren.module.scss';
+import * as templateSideNavStyles from  '../../templates/sideNav/sideNav.module.scss';
 import * as moduleSignInStyles from '../../modules/credentialsForm/credentialsForm.module.scss';
 import * as componentPrimaryBtnStyles from '../../components/primaryBtn/primaryBtn.module.scss';
 import * as componentTextFieldStyles from '../../components/textField/textField.module.scss';
@@ -12,6 +13,7 @@ import * as componentAvatarStyles from '../../components/avatar/avatar.module.sc
 // 1 - generate context
 const ctx = {
   templateStyles,
+  templateSideNavStyles,
   moduleSignInStyles,
   componentPrimaryBtnStyles,
   componentTextFieldStyles,
