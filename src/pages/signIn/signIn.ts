03f523516e7b8794419d2dc92ea791bbd9022c2b
krasnokutskiy.e <Krasnokutskiy.E@ostec-group.ru>
2021-12-07 16:31:29 +0300

    WIP

--- src/pages/signIn/signIn.ts
+++ src/pages/signIn/signIn.ts
@@ -1,43 +1,59 @@
-import compileTemplate from './signIn.pug'
-
-// styles
+// asssets import
 import '../../assets/styles/index.scss'
 
+// components import (.ts)
+import CenterContent from '../../templates/centerContent/centerContent'
+import Form from '../../modules/form/form'
+import TextField from '../../components/textField/textField'
+import PriamryBtn from '../../components/primaryBtn/primaryBtn'
+
 // 1 - generate context
-const ctx = {
-  parameters: {
-    title: 'Sign In',
-    linkTo: '/createAccount.html',
-    linkMessage: 'Create account?'
+const formProps = {
+  title: 'Sign In',
+  linkTo: '/createAccount.html',
+  linkMessage: 'Create account?'
+}
+
+const textFieldProps = [
+  {
+    label: 'Username',
+    type: 'text',
+    name: 'login',
+    id: 'login',
+    placeholder: 'Username',
+    required: 'required'
   },
-  textFieldParameters: [
-    {
-      label: 'Username',
-      type: 'text',
-      name: 'login',
-      id: 'login',
-      placeholder: 'Username',
-      required: 'required'
-    },
-    {
-      label: 'Password',
-      type: 'text',
-      name: 'password',
-      id: 'password',
-      placeholder: 'Password',
-      required: 'required'
-    }
-  ],
-  primaryBtnParameters: {
-    text: 'Sign In',
-    type: 'submit',
-    classes: ['mt-auto']
+  {
+    label: 'Password',
+    type: 'text',
+    name: 'password',
+    id: 'password',
+    placeholder: 'Password',
+    required: 'required'
+  }
+]
+
+const primaryBtnProps = {
+  text: 'Sign In',
+  type: 'submit',
+  classes: ['mt-auto'],
+  events: {
+    click: (event: any) => console.log('OLD 0 EVENT=', event)
   }
 }
 
-// 2 - generate markup
+// 2 - create page structure
+const page = new CenterContent({
+  content: new Form({
+    ...formProps,
+    childrenList: textFieldProps.map(p => new TextField(p)),
+    submitBtn: new PriamryBtn(primaryBtnProps)
+  })
+})
+
+// 3 - generate markup
 const app: HTMLElement | null = document.getElementById('app')
 if (app !== null) {
   app.innerHTML = ''
-  app.insertAdjacentHTML('afterbegin', compileTemplate(ctx))
+  app.appendChild(page.render())
 }
