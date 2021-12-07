9004fda29d743718c47cc5af84ec8236b5627b78
krasnokutskiy.e <Krasnokutskiy.E@ostec-group.ru>
2021-12-08 02:38:01 +0300

    WIP

--- src/helpers/validate.ts
+++ src/helpers/validate.ts
@@ -2,8 +2,6 @@ export default (event: Event): void => {
   const tgt = event.target
   const isValid = tgt.validity.valid
 
-  console.log('----event=', event)
-
   if (isValid === false) {
     tgt.nextSibling.classList.remove('hidden')
   } else {
