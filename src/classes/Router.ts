27c526d8f86d74b5d80bc7a69a395ef1941cbb78
krasnokutskiy.e <Krasnokutskiy.E@ostec-group.ru>
2022-05-06 15:48:26 +0300

    WIP

--- src/classes/Router.ts
+++ src/classes/Router.ts
@@ -3,60 +3,39 @@ import { Block } from './Block'
 import store from '../store'
 
 export default class Router {
+  /* class properties */
+  static _instance: Router
+  readonly _pathnames: string[]
+  private _currentRoute: Route | null
+  private _unprotectedPaths: string[]
+  readonly _rootQuery: string
+  private _onRouteCallback: () => Promise<void>
   public routes: Route[]
   public history: History
-  private _currentRoute: Route | null
-  readonly _rootQuery: string
-  readonly _pathnames: string[]
-  private _onRouteCallback: () => Promise<void>
-  private _unprotectedPaths: string[]
-  static __instance: Router
 
+  /* constructor */
   constructor (rootQuery: string) {
-    if (Router.__instance) {
-      return Router.__instance
+    if (Router._instance) {
+      return Router._instance
     }
 
-    this.routes = []
-    this._pathnames = []
-    this._unprotectedPaths = []
-    this.history = window.history
     this._currentRoute = null
     this._rootQuery = rootQuery
+    this._pathnames = []
+    this._unprotectedPaths = []
     this._onRouteCallback = async () => {}
+    this.history = window.history
+    this.routes = []
 
-    Router.__instance = this
+    Router._instance = this
   }
 
+  /* getters */
   get currentRoute (): Route | null {
     return this._currentRoute
   }
 
-  public use (pathname: string, block: typeof Block): Router {
-    const route = new Route(pathname, block, { rootQuery: this._rootQuery })
-
-    this.routes.push(route)
-    this._pathnames.push(pathname)
-    return this
-  }
-
-  private _hasRoute (pathname: string): string {
-    if (!this._pathnames.includes(pathname)) {
-      return '*'
-    }
-    return pathname
-  }
-
-  public start (): void {
-    window.onpopstate = async () => {
-      const pathname = this._hasRoute(window.location.pathname)
-      void this._onRoute(pathname)
-    }
-
-    const pathname = this._hasRoute(window.location.pathname)
-    void this._onRoute(pathname)
-  }
-
+  /* private methods */
   private async _onRoute (pathname: string): Promise<void> {
     store.shift('flow:store-did-update')
     const route = this.getRoute(pathname)
@@ -80,9 +59,30 @@ export default class Router {
     store.setState('route', { name: pathname })
   }
 
-  public onRoute (callback: () => Promise<void>): Router {
-    this._onRouteCallback = callback
+  private _hasRoute (pathname: string): string {
+    if (!this._pathnames.includes(pathname)) {
+      return '*'
+    }
+    return pathname
+  }
+
+  /* public methods */
+  public use (pathname: string, block: typeof Block): Router {
+    const route = new Route(pathname, block, { rootQuery: this._rootQuery })
+
+    this.routes.push(route)
+    this._pathnames.push(pathname)
     return this
+  }
+
+  public start (): void {
+    window.onpopstate = async () => {
+      const pathname = this._hasRoute(window.location.pathname)
+      void this._onRoute(pathname)
+    }
+
+    const pathname = this._hasRoute(window.location.pathname)
+    void this._onRoute(pathname)
   }
 
   public setUnprotectedPaths (paths: string[]): Router {
@@ -90,24 +90,29 @@ export default class Router {
     return this
   }
 
-  public go (pathname: string): void {
-    this.history.pushState({}, '', pathname)
-    void this._onRoute(pathname)
-  }
-
-  public back (): void {
-    this.history.back()
+  public onRoute (callback: () => Promise<void>): Router {
+    this._onRouteCallback = callback
+    return this
   }
 
   public forward (): void {
     this.history.forward()
   }
 
-  public getRoute (pathname: string): Route | undefined {
-    return this.routes.find(route => route.match(pathname))
+  public back (): void {
+    this.history.back()
+  }
+
+  public go (pathname: string): void {
+    this.history.pushState({}, '', pathname)
+    void this._onRoute(pathname)
   }
 
   public getLocationPathname (): string {
     return window.location.pathname
+  }
+
+  public getRoute (pathname: string): Route | undefined {
+    return this.routes.find(route => route.match(pathname))
   }
 }
